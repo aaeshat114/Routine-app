1d2884dfa3aa708f7d01756e8d216a9e9d30e5a6
aaeshat114 <aaeshat114@gmail.com>
2026-05-31 22:44:12 -0600

    Add preloaded images array for tasks

--- app.js
+++ app.js
@@ -3,6 +3,31 @@
 // ==========================================
 const DB_NAME = 'KidsRoutineDB';
 const DB_VERSION = 1;
+const PRELOADED_IMAGES = [
+  './assets/images/task1.png',
+  './assets/images/task2.png',
+  './assets/images/task3.png',
+  './assets/images/task4.png',
+  './assets/images/task5.png',
+  './assets/images/task6.png',
+  './assets/images/task7.png',
+  './assets/images/task8.png',
+  './assets/images/task9.png',
+  './assets/images/task10.png',
+  './assets/images/task11.png',
+  './assets/images/task12.png',
+  './assets/images/task13.png',
+  './assets/images/task14.png',
+  './assets/images/task15.png',
+  './assets/images/task16.png',
+  './assets/images/task17.png',
+  './assets/images/task18.png',
+  './assets/images/task19.png',
+  './assets/images/task20.png',
+];
+let currentTaskImageIndex = null;
+
+
 let db;
 
 function initDB() {
