fe171aa8fb7a4574eadb086d8c721390b82f1d1f
aaeshat114 <aaeshat114@gmail.com>
2026-05-31 22:39:55 -0600

    Include task images in service worker cache
    Add image assets for tasks to the service worker cache.

--- sw.js
+++ sw.js
@@ -10,6 +10,26 @@ const ASSETS = [
   './assets/audio/sfx-complete.mp3',
   './assets/audio/sfx-warning.mp3',
   './assets/audio/sfx-victory.mp3'
+  './assets/images/task1.png',
+  './assets/images/task2.png',
+  './assets/images/task3.png',
+  './assets/images/task4.png',
+  './assets/images/task5.png',
+  './assets/images/task6.png',
+  './assets/images/task7.png',
+  './assets/images/task8.png',
+  './assets/images/task9.png',
+  './assets/images/task10.png',
+  './assets/images/task11.png',
+  './assets/images/task12.png',
+  './assets/images/task13.png',
+  './assets/images/task14.png',
+  './assets/images/task15.png',
+  './assets/images/task16.png',
+  './assets/images/task17.png',
+  './assets/images/task18.png',
+  './assets/images/task19.png',
+  './assets/images/task20.png',
 ];
 
 self.addEventListener('install', event => {
